d6585481d48414b2ee5fd5f0bc5dbc351178fd7c
Tanvir Parvez <prottoy15-5378@diu.edu.bd>
2026-05-11 00:18:58 +0600

    updated portfolio sections

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -93,7 +93,7 @@ function Education() {
           </p>
 
           <span className="edu-location">
-            Bangladesh
+            Khulna,Bangladesh
           </span>
 
         </div>
@@ -131,7 +131,7 @@ function Education() {
           </p>
 
           <span className="edu-location">
-            Bangladesh
+            Satkhira, Khulna, Bangladesh
           </span>
 
         </div>
